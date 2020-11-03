5e083e1ef8579dd6c5a8a313d4608c794a187d13
Paula Cruz <p242776@dac.unicamp.br>
2020-11-03 11:27:12 -0300

    projeto dos cubos e relatório

---
+++ Tentativa dos Cubos/Assets/Scripts/controle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class controle : MonoBehaviour
+{
+    public Vector3 direcao;
+    public float velocidade;
+    Rigidbody rb;
+
+    public Material azul;
+    public Material normal;
+    public Material vermelho;
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direcao = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.Translate(direcao * velocidade * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
